7ec0d46424806fe11e301e7b0f2c212e1e874927
dredu7 <evgeni@ultimate.money>
2024-03-16 19:10:36 +0100

    update fetching token symbols from routescan to spicy explorer api

--- frontend/src/lib/reader.ts
+++ frontend/src/lib/reader.ts
@@ -1,42 +1,39 @@
 import axios from 'axios';
 
 interface Token {
-  chainId: string;
-  tokenAddress: string;
-  holderBalance: string;
-  valueInUsd: string;
-  token: {
-    name: string;
-    symbol: string;
-    decimals: number;
-    dexCheck: boolean;
-    detail: any;
-  };
+  balance: string;
+  contractAddress: string;
+  decimals: string;
+  name: string;
+  symbol: string;
+  type: string;
 }
 
 interface ApiResponse {
-  items: Token[];
-  link: {
-    next: string;
-    nextToken: string;
-  };
+  message: string;
+  result: Token[];
+  status: string;
 }
 
 export const fetchTokenSymbols = async (walletAddress: string): Promise<string[]> => {
-  const url = `https://cdn.testnet.routescan.io/api/evm/all/address/${walletAddress}/erc20-holdings?includedChainIds=88882&limit=100`;
+  const url = `https://spicy-explorer.chiliz.com/api?module=account&action=tokenlist&address=${walletAddress}`;
   
   try {
-    const response = await axios.get<ApiResponse>(url);
-    const tokens = response.data.items;
-    const symbols = tokens.map(token => token.token.symbol);
-    return symbols;
+    const response = await axios.get<ApiResponse>(url, { headers: { accept: 'application/json' } });
+    if (response.data.message === "OK") {
+      const symbols = response.data.result.map(token => token.symbol);
+      return symbols;
+    } else {
+      console.error('Error fetching token symbols:', response.data.message);
+      return [];
+    }
   } catch (error) {
     console.error('Error fetching token symbols:', error);
     return [];
   }
 };
 
-// Example usage:
+// Example usage
 const walletAddress = '0x08Ab1Ce3686cb7E616af2D3E068356B160c4c038';
 fetchTokenSymbols(walletAddress)
   .then(symbols => console.log('Symbols:', symbols))
