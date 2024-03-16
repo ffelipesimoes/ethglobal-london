f1e1043535e640e7eddf0d795013d716e73b1e90
Felipe Simões <ffelipesimoes@gmail.com>
2024-03-16 11:40:36 +0000

    add fan tokens on page

---
+++ frontend/src/lib/mocks/cards.ts
@@ -0,0 +1,45 @@
+export const fanTokens = [
+    {
+      "id": "barcelona",
+      "name": "Barcelona FC",
+      "description": "Join to connect with fans of Barcelona FC and engage in the community.",
+      "members": 19000,
+      "image": "/barcelona.png"
+    },
+    {
+      "id": "juventus",
+      "name": "Juventus FC",
+      "description": "A place for devoted Juventus FC supporters to meet and share their passion.",
+      "members": 12000,
+      "image": "/juventus.png"
+    },
+    {
+      "id": "psg",
+      "name": "Paris Saint-Germain",
+      "description": "Paris Saint-Germain fans can gather here to discuss all things PSG.",
+      "members": 25000,
+      "image": "/psg.png"
+    },
+    {
+      "id": "manutd",
+      "name": "Manchester United",
+      "description": "The hub for all Manchester United fans to talk about their favourite team.",
+      "members": 30000,
+      "image": "/manutd.png"
+    },
+    {
+      "id": "atletico",
+      "name": "Atletico Madrid",
+      "description": "Connect with fellow Atletico Madrid fans and join the conversation.",
+      "members": 8000,
+      "image": "/atletico.png"
+    },
+    {
+      "id": "milan",
+      "name": "AC Milan",
+      "description": "A community for AC Milan fans to share their love for the team.",
+      "members": 16000,
+      "image": "/milan.png"
+    }
+  ]
+  
