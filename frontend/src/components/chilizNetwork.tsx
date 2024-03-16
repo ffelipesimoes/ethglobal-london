60ee795759073f30706ac47ecf380b9a573d4993
Eldar Agayev <theeldarwes@gmail.com>
2024-03-16 14:43:41 +0000

    Added logo to chiliz

--- frontend/src/components/chilizNetwork.tsx
+++ frontend/src/components/chilizNetwork.tsx
@@ -21,7 +21,7 @@ export const chilizWallet = [
     blockExplorerUrls: ['https://testnet.chiliscan.com/'], // Spicy Block Explorer URL
     chainId: 88882, // Chain ID for Spicy Testnet
     chainName: 'Chiliz Spicy Testnet',
-    iconUrls: ['https://path-to-your-chiliz-network-icon'], // You might want to host an appropriate icon and link it here
+    iconUrls: ['https://s2.coinmarketcap.com/static/img/coins/200x200/4066.png'], // You might want to host an appropriate icon and link it here
     name: 'Chiliz Spicy Testnet',
     nativeCurrency: {
       decimals: 18, // Assuming 18 decimal places, adjust if different
