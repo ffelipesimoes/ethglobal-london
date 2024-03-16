0d982193d815e862453eaea7ee882eaea8d9dc25
dredu7 <evgeni@ultimate.money>
2024-03-16 22:03:52 +0100

    small test adjustments

--- contracts/test/FanTokenBuilder.t.sol
+++ contracts/test/FanTokenBuilder.t.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import "forge-std/Test.sol";
-import "../src/FanTokenBuilder.sol";
+import {Test} from "forge-std/Test.sol";
+import {FanTokenBuilder} from "../src/FanTokenBuilder.sol";
 
 contract FanTokenBuilderTest is Test {
     FanTokenBuilder public token;
--- contracts/test/Lock.t.sol
+++ contracts/test/Lock.t.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {Test, console2} from "forge-std/Test.sol";
+import {Test} from "forge-std/Test.sol";
 import {Lock} from "../src/Lock.sol";
 import "lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 
