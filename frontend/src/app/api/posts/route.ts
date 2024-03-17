081bded8e604f8c6b9e608e4da921082dd0984cd
Eldar Agayev <theeldarwes@gmail.com>
2024-03-17 02:14:54 +0000

    Added fetching and API

--- frontend/src/app/api/posts/route.ts
+++ frontend/src/app/api/posts/route.ts
@@ -4,27 +4,25 @@ import connect from "../../../lib/db"; // Adjust path as needed
 import Post from "../../../models/Post"; // Adjust path as needed
 import { NextResponse } from "next/server";
 
-export async function POST(req: Request) {
+export async function GET(req: Request) {
   // Ensure database connection
   await connect();
 
   // This is a json data from the request body
-  const data = await req.json();
-  console.log(data);
+  const data = req;
   try {
     // Create a new post using the Post model
-    const newPost = await Post.create({
-      content: data.content,
-      community: data.community,
-      author: data.author,
-      createdAt: new Date(),
-    });
+    const posts = await Post.find({
+      community: "Barcelona",
+    }).sort({ createdAt: -1 });
 
-    return new Response(JSON.stringify(newPost), {
+    console.log(posts[0].content)
+
+    return new Response(JSON.stringify(posts), {
       headers: {
         "content-type": "application/json",
-        },
-        });
+      },
+    });
   } catch (error) {
     // Handle errors, e.g., missing required fields
     return NextResponse.json({ success: false, error });
