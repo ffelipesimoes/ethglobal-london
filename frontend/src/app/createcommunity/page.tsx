31c4cdd7b66b73d627b2c12d306af50af191eb81
Eldar Agayev <theeldarwes@gmail.com>
2024-03-17 04:24:41 +0000

    Added IPFS

---
+++ frontend/src/app/createcommunity/page.tsx
@@ -0,0 +1,14 @@
+// Import necessary dependencies
+import React from 'react';
+import FileUploadToIPFS from '../../components/Ipfs'; // Adjust the import path as necessary
+
+// Define the page component
+const FileUploadPage = () => {
+  return (
+    <div>
+      <FileUploadToIPFS />
+      </div>
+  );
+};
+
+export default FileUploadPage;
