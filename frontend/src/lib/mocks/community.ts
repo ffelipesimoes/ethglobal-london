9bb28047096093cfb118e5b52597de3390a7f202
Felipe Simões <ffelipesimoes@gmail.com>
2024-03-17 04:41:20 +0000

    fix: fix hidration error

--- frontend/src/lib/mocks/community.ts
+++ frontend/src/lib/mocks/community.ts
@@ -2,7 +2,7 @@ export const communityData = [
   {
     id: 1,
     name: "FC Barcelona",
-    membersCount: 19000,
+    membersCount: 19.000,
     description:
       "Discover the vibrant community of FC Barcelona fans and join the movement towards innovative funding solutions for your favorite team. Engage with fellow supporters, participate in discussions, and stay updated with the latest initiatives powered by web3 technology.",
     imageBanner: "/barcelona.png", // Presumindo uma nova imagem de banner
@@ -13,7 +13,7 @@ export const communityData = [
   {
     id: 2,
     name: "Juventus FC",
-    membersCount: 12000,
+    membersCount: 12.000,
     description:
       "A place for devoted Juventus FC supporters to meet and share their passion. Explore a unique space where fans can exchange ideas, celebrate victories, and support the team through thick and thin.",
     imageBanner: "/juventus.png", // Presumindo uma nova imagem de banner
@@ -24,7 +24,7 @@ export const communityData = [
   {
     id: 3,
     name: "Paris Saint-Germain",
-    membersCount: 25000,
+    membersCount: 25.000,
     description:
       "Join the Paris Saint-Germain community to get closer to your favorite team. Engage in discussions about matches, strategies, and the future of the team, all while supporting PSG's journey with web3 technology.",
     imageBanner: "/psg.png", // Presumindo uma nova imagem de banner
@@ -35,7 +35,7 @@ export const communityData = [
   {
     id: 4,
     name: "Manchester United",
-    membersCount: 30000,
+    membersCount: 30.000,
     description:
       "Be part of the Manchester United community and support your team directly through web3 technology. Engage with passionate fans, share your thoughts on match strategies, and get the latest news on Manchester United.",
     imageBanner: "/manutd.png", // Uma imagem de banner hipotética
@@ -46,7 +46,7 @@ export const communityData = [
   {
     id: 5,
     name: "Atletico Madrid",
-    membersCount: 8000,
+    membersCount: 8.000,
     description:
       "Join the ranks of Atletico Madrid supporters by engaging with the community. Dive into discussions about team dynamics, upcoming matches, and how web3 is revolutionizing the way we support our teams.",
     imageBanner: "/atletico.png", // Uma imagem de banner hipotética
@@ -57,7 +57,7 @@ export const communityData = [
   {
     id: 6,
     name: "AC Milan",
-    membersCount: 16000,
+    membersCount: 16.000,
     description:
       "The AC Milan community is a vibrant space for fans to gather and share their passion for the team. Learn about AC Milan's latest web3 initiatives, discuss the team's performance, and show your support by holding AC Milan tokens.",
     imageBanner: "/milan.png", // Uma imagem de banner hipotética
